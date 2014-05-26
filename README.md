3975a7f248c9bc8e5a250fae7e7ac5102587a631
Corey Norris <coreypnorris@gmail.com>
2014-05-26 13:29:31 -0700

    refactor readme

---
+++ README.md
@@ -0,0 +1,7 @@
+##Legacy data
+
+####This is a ruby method for transforming a hash whose values are arrays, and transforming it into a hash where each element in the arrays has its value as the key of the array to which it originally belonged
+
+To use it just copy the code from the lib/legacy_data.rb file and paste it into a ruby client ([Here's a link](http://www.learnhowtoprogram.com/lessons/installing-ruby) that helped me install ruby on my machine). Then type ‘legacy_data(your-hash-here)' in the client.
+
+We built this project while we were students at [Epicodus](http://www.epicodus.com/). [This exercise](http://www.learnhowtoprogram.com/lessons/legacy-data-conversion-prime-sifting-nth-prime) was meant to introduce us to writing our own ruby methods.
--- lib/legacy_data.rb
+++ lib/legacy_data.rb
@@ -1,15 +1,5 @@
-legacy_hash = { 
-  1  => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
-  2  => ["D", "G"],
-  3  => ["B", "C", "M", "P"],
-  4  => ["F", "H", "V", "W", "Y"],
-  5  => ["K"],
-  8  => ["J", "X"],
-  10 => ["Q", "Z"]
-}
-
 def legacy_data(hash)
-  
+
 
   reverse_hash = hash.invert
   new_hash = {}
@@ -23,4 +13,21 @@ def legacy_data(hash)
   new_hash
 end
 
-puts legacy_data(legacy_hash)
+# example_hash = {
+#   1  => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+#   2  => ["D", "G"],
+#   3  => ["B", "C", "M", "P"],
+#   4  => ["F", "H", "V", "W", "Y"],
+#   5  => ["K"],
+#   8  => ["J", "X"],
+#   10 => ["Q", "Z"]
+# }
+
+# The example hash should turn out like this
+# { "A" => 1, "B" => 3,  "C" => 3, "D" => 2, "E" => 1,
+#   "F" => 4, "G" => 2,  "H" => 4, "I" => 1, "J" => 8,
+#   "K" => 5, "L" => 1,  "M" => 3, "N" => 1, "O" => 1,
+#   "P" => 3, "Q" => 10, "R" => 1, "S" => 1, "T" => 1,
+#   "U" => 1, "V" => 4,  "W" => 4, "X" => 8, "Y" => 4,
+#   "Z" => 10
+# }
